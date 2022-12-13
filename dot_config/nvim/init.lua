e545f7cd0555536a1efb065150dfe0b89b346d47
Marius Ihring <marius.ihring@icloud.com>
2022-12-13 15:21:16 +0100

    added dotfiles as aliascommand to go into chezmoi folder

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -41,6 +41,10 @@ require 'nvim-treesitter.configs'.setup {
         -- list of language that will be disabled
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 
+
+
+        -- test lalalala
+        -- re-add test lalala
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
